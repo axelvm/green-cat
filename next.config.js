c3ca52ded5101e922b8a94ff33516434aec91d2f
axelvm <1887she@gmail.com>
2023-03-24 15:00:06 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
-    return [
-      {
-        "Access-Control-Allow-Methods":
-          "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-        "Access-Control-Allow-Origin": "*",
-      },
-    ];
+    return {
+      source: "*",
+      headers: [
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+        },
+        {
+          key: "Access-Control-Allow-Origin",
+          value: "*",
+        },
+      ],
+    };
   },
 };
 
