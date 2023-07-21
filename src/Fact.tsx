4f04f3933b162074d468e4fb8eb82c0b1dcd579e
Axel Vanet-Mas <axel.vanetmas@sensefuel.com>
2023-07-21 11:09:47 +0200

    test

--- src/Fact.tsx
+++ src/Fact.tsx
@@ -1,26 +1,21 @@
-import React from "react"
-import styles from '@/styles/Fact.module.css'
+import React from "react";
+import styles from "@/styles/Fact.module.css";
 
 export default function Fact() {
-const [fact, setFact] = React.useState("")
-React.useEffect(() => {
-
-	(async () => {
-		const request = await fetch('https://green-rledqqi9m-axelvm.vercel.app/api/fact')
-		const response = await request.json()
-		setFact(response.fact)
-	})()
-})
-
+  const [fact, setFact] = React.useState("");
+  React.useEffect(() => {
+    (async () => {
+      const request = await fetch(
+        "https://green-rledqqi9m-axelvm.vercel.app/api/fact"
+      );
+      const response = await request.json();
+      setFact(response.fact);
+    })();
+  });
 
   return (
     <>
-		<div className={styles.fact}>
-			fact: {fact}
-		</div>
+      <div className={styles.fact}>fact: {fact} coucou</div>
     </>
-  )
+  );
 }
-
-
-
